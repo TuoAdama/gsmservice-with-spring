c7d5c1ac46fec6c7c593f1bc1b8edcd0e17a2aeb
TuoAdama <tuoadama17@gmail.com>
2022-04-21 09:57:18 +0000

    [fix] fixing search box

--- src/main/java/com/example/main/services/TransfertService.java
+++ src/main/java/com/example/main/services/TransfertService.java
@@ -19,7 +19,6 @@ import javax.validation.Validator;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.main.entities.Etat;
